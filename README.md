54e4b2737fde74e7072e5bb3b5fc30685a7d54d4
Desmond Kyeremeh <elDekyfin@gmail.com>
2023-05-03 10:57:43 +0000

    Allow mode to be configured

--- README.md
+++ README.md
@@ -1,25 +1,32 @@
 # elFinder Picker
+
 Enable your website users to select files using elFinder File Browser. You can also use this tool as a file picker module for tinyMCE editor
 
 ## Demo
+
 Coming soon
 
 ## Dependencies
+
 - jQuery
 - [elFinder](https://github.com/Studio-42/elFinder)
 
 ## Installation
 
 ### Bower
+
 Run `bower install elfinder-picker`
 
 ### NPM
+
 Run `npm i -s elfinder-picker`
 
 ### Yarn
+
 Run `yarn add elfinder-picker`
 
 ### Manual Install
+
 Downoad [elFinder Picker](https://github.com/dkyeremeh/elfinder-picker/archive/master.zip)
 
 ### Add to Project
@@ -28,20 +35,30 @@ Downoad [elFinder Picker](https://github.com/dkyeremeh/elfinder-picker/archive/m
 
 ```html
 <script src="/path/to/elFinderPicker.js"></script>
-<link type="text/css" rel="stylesheet" href="/path/to/elFinderPicker.css">
+<link type="text/css" rel="stylesheet" href="/path/to/elFinderPicker.css" />
 ```
+
 #### Configure elFinder Picker as shown
 
 ```html
-<script>elFinderPicker.config( { url: "path/to/elfinder.html" })</script>
+<script>
+  elFinderPicker.config({ url: 'path/to/elfinder.html' });
+</script>
 ```
 
-### Configure elFinder 
+### Configure elFinder
 
 ```javascript
+
+// We use this to configure whether double-clicking should open or select file
+var isOpenMode =
+	!window.frameElement ||
+	window.frameElement.getAttribute("data-mode") === "open";
+
+// Configuration for elFinder
 {
-	url : '/path/to/elfinder/connector.php', 
-	getFileCallback : function(file, fm) {
+	url : '/path/to/elfinder/connector.php',
+	getFileCallback : isOpenMode ? undefined : function(file, fm) {
 		parent.elFinderPicker.oninsert(file, fm);
 	}
 }
@@ -50,10 +67,12 @@ Downoad [elFinder Picker](https://github.com/dkyeremeh/elfinder-picker/archive/m
 ## Usage
 
 ### Using elFinder Picker Directly
+
 `elFinderPicker.open( callback )`
 The callback function takes two arguments:
 
 #### Example
+
 ```html
 <button onclick="elFinderPicker.open( function( url, info ){  $("#input").val( url ) } )">Pick File</button>
 ```
@@ -62,9 +81,9 @@ The callback function takes two arguments:
 
 ```javascript
 tinymce.init({
-	selector: '.text-editor',
-	height: 350,
-	plugins: [ ],
-	file_picker_callback: elFinderPicker.open,
+  selector: '.text-editor',
+  height: 350,
+  plugins: [],
+  file_picker_callback: elFinderPicker.open,
 });
 ```
--- elFinderPicker.js
+++ elFinderPicker.js
@@ -1,83 +1,84 @@
-"use strict";
+'use strict';
 
-(function( $ ){
+(function ($) {
+  var settings = {
+    url: '',
+  };
 
-	var settings = {
-		url: "",
-	}
+  var callback, meta, value, popup;
 
-	var callback, meta, value, popup;
+  window.elFinderPicker = {
+    open: function (cb, v, m) {
+      callback = cb;
+      value = v;
+      meta = m;
 
-	window.elFinderPicker = {
-		open: function ( cb, v, m ) {
-			callback = cb;
-			value = v;
-			meta = m;
-
-			if( popup ){
-				popup.show();
-			}
-			else{
-				popup = $( "<div class='elfinder-picker'>\
+      if (popup) {
+        popup.show();
+      } else {
+        popup = $(
+          "<div class='elfinder-picker'>\
 					<div class='elfinder-picker-backdrop'></div>\
 					<div class='elfinder-picker-content'>\
 						<h3 class='elfinder-picker-title'>File Manager</h3>\
 						<div class='elfinder-picker-close'>&times;</div>\
-						<iframe class='elfinder-picker-iframe' src='" + settings.url + "' allowfullscreen></iframe>\
+						<iframe class='elfinder-picker-iframe' src='" +
+            settings.url +
+            "' allowfullscreen data-mode='select'></iframe>\
 					</div>\
-				</div>" );
-				$( document.body ).append( popup );
-				popup.find(".elfinder-picker-close, .elfinder-picker-backdrop").click( function(){ popup.hide() } );
-			}
-			return popup;
-		},
-		config: function( config ){
+				</div>"
+        );
+        $(document.body).append(popup);
+        popup
+          .find('.elfinder-picker-close, .elfinder-picker-backdrop')
+          .click(function () {
+            popup.hide();
+          });
+      }
+      return popup;
+    },
+    config: function (config) {
+      var fields = ['url'];
 
-			var fields = [ "url" ];
+      fields.forEach(function (field) {
+        if (config[field]) {
+          settings[field] = config[field];
+        }
+      });
+    },
+    oninsert: function (file) {
+      var url, reg, info;
 
-			fields.forEach( function( field ){
-				if ( config[ field ] ){
-					settings[ field ] = config[ field ]; 
-				}
-			});
-		},
-		oninsert: function( file ){
-			var url, reg, info;
+      meta = meta || { filetype: 'file' };
 
-			meta = meta || { filetype: "file" };
+      // URL normalization
+      url = file.url;
+      reg = /\/[^/]+?\/\.\.\//;
+      while (url.match(reg)) {
+        url = url.replace(reg, '/');
+      }
 
-			// URL normalization
-			url = file.url;
-			reg = /\/[^/]+?\/\.\.\//;
-			while(url.match(reg)) {
-			url = url.replace(reg, '/');
-			}
+      // Make file info
+      info = file.name;
 
-			// Make file info
-			info = file.name;
+      // Provide file and text for the link dialog
+      if (meta.filetype == 'file') {
+        callback(url, { text: info, title: info });
+      }
 
-			// Provide file and text for the link dialog
-			if (meta.filetype == 'file') {
-				callback(url, { text: info, title: info });
-			}
+      // Provide image and alt text for the image dialog
+      if (meta.filetype == 'image') {
+        callback(url, { alt: info });
+      }
 
-			// Provide image and alt text for the image dialog
-			if (meta.filetype == 'image') {
-				callback(url, {alt: info});
-			}
-
-			// Provide alternative source and posted for the media dialog
-			if (meta.filetype == 'media') {
-				callback(url);
-			}
-			this.close();
-
-		},
-		close: function(){
-			popup.hide();
-		}
-
-	}
-
-})( jQuery )
-	
+      // Provide alternative source and posted for the media dialog
+      if (meta.filetype == 'media') {
+        callback(url);
+      }
+      this.close();
+    },
+    close: function () {
+      popup.hide();
+    },
+  };
+})(jQuery);
