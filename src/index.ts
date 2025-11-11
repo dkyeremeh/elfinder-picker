0a144e08b65d888638b975f0d6c68f1e2db524dd
Desmond Kyeremeh <elDekyfin@gmail.com>
2025-11-11 10:54:59 +0000

    Fix issues importing css

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import './styles.css';
 
 export type * from './types';
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // Check if building library or demo
 const buildLibrary = process.env.BUILD_TARGET === 'lib';
 
 export default defineConfig({
+  plugins: buildLibrary ? [cssInjectedByJsPlugin()] : [],
   root: buildLibrary ? './' : 'demo',
   publicDir: buildLibrary ? false : 'demo',
   build: buildLibrary ? {
